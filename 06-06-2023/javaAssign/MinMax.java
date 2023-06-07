dcfaa738c6ec44785e3dc201be877dc545fbbe15
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-06-07 10:05:26 -0500

    added 6-6 Assinments

---
+++ 06-06-2023/javaAssign/MinMax.java
@@ -0,0 +1,25 @@
+package Test2;
+
+public class MinMax {
+
+    public static void main(String[] args) {
+        fMinMax(new int[]{2, 5, 8, 1, 16, 23});
+    }
+    static void fMinMax(int[] arr){
+        int s = arr[0];
+        int l = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < s) {
+                s = arr[i];
+            }
+            if (arr[i] > l) {
+                l = arr[i];
+            }
+        }
+
+        System.out.println("Smallest is: " + s);
+        System.out.println("Largest is: " + l);
+    }
+
+}
