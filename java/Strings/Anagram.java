61d23ea041ea3f0552ad8a0e57bfd7209c7e4771
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-06-08 14:10:17 -0500

    updated java

---
+++ java/Strings/Anagram.java
@@ -0,0 +1,23 @@
+package Test2;
+
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String s1 = "srihari";
+        String s2 = "srriiah";
+        System.out.println(checkAnagram(s1,s2));
+
+    }
+    static boolean checkAnagram(String s1,String s2){
+        if(s1.length() != s2.length())return false;
+        char[] c1 = s1.toLowerCase().toCharArray();
+        char[] c2 = s2.toLowerCase().toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        return Arrays.equals(c1,c2);
+    }
+
+}
---
+++ java/Strings/RevEWord.java
@@ -0,0 +1,25 @@
+package Test2;
+
+import java.util.Arrays;
+
+public class RevEWord {
+    public static void main(String[] args) {
+        String s1="this is srihari";
+        printRev(s1);
+
+    }
+
+    static void printRev(String s1){
+        String[] c1 = s1.split(" ");
+        String temp = "";
+        String revString = "";
+        for(int i=0;i<c1.length;i++){
+            for(int j=c1[i].length()-1;j>=0;j--){
+                temp+=c1[i].charAt(j);
+            }
+            revString+=temp+" ";
+            temp="";
+        }
+        System.out.println(revString);
+    }
+}
