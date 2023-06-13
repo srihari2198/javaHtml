c93a2eff5ea3e112ef35c18c3456843c93555241
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-06-13 15:25:26 -0500

    Update ConToInt.java

--- java/ExceptionHandling/ConToInt.java
+++ java/ExceptionHandling/ConToInt.java
@@ -3,18 +3,18 @@ package ExceptionHandling;
 public class ConToInt {
 
     public static void main(String[] args) {
-        String s = " ";
+        String s = "123f";
         try {
             System.out.println(converToInt(s));
 
         }
         catch (NumberFormatException e) {
-            System.out.println(e);
+            System.out.println(e+"   Cannot convert  String to integer");
         }
 
     }
 
-    static int converToInt(String s){
+    static int converToInt(String s) throws ArithmeticException{
         return Integer.parseInt(s);
     }
 }
