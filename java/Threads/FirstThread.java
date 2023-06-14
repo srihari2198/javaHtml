8612466acfedc299e6a5ae75c0504ec8e09bba62
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-06-14 11:24:23 -0500

    .

---
+++ java/Threads/FirstThread.java
@@ -0,0 +1,15 @@
+package Test2;
+
+public class FirstThread extends Thread{
+
+    public void run(){
+        for(int i=1;i<11;i++){
+            System.out.print(" " +i);
+        }
+    }
+
+    public static void main(String[] args) {
+        FirstThread ft = new FirstThread();
+         ft.start();
+    }
+}
