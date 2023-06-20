84c196c044bf90ff69e231cf69ec5b35131e3801
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-06-20 14:15:31 -0500

    Update PrintMoreThanAverage.java

--- java/Collections/PrintMoreThanAverage.java
+++ java/Collections/PrintMoreThanAverage.java
@@ -4,6 +4,7 @@ import Test2.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Stream;
 
 public class PrintMoreThanAverage {
@@ -16,8 +17,9 @@ public class PrintMoreThanAverage {
         l.add(new Student("Sri","hari",2.8f));
 
 
-        System.out.println(l.stream().mapToDouble(s -> s.gpa).average());
+        double avg = l.stream().mapToDouble(s -> s.gpa).average().getAsDouble();
 
+        l.stream().filter(i -> (double)i.gpa> avg).forEach(System.out::println);
 
 
     }
