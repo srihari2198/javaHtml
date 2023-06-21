5da144b97861dcccb443ae9b2d736495d661a56f
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-06-21 10:45:50 -0500

    Update ArraySum.java

--- java/Collection[Map]/ArraySum.java
+++ java/Collection[Map]/ArraySum.java
@@ -7,13 +7,10 @@ public class ArraySum {
         int[] arr = {10,20,30,40,50,60};
         int target = 90;
         HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
+        for(int i=0;i<arr.length;i++)
+            for(int j=i+1;j<arr.length;j++)
+                if(arr[i]+arr[j]==target)
                     hm.put(i,j);
-                }
-            }
-        }
         hm.forEach((k,v) -> System.out.println(k+" "+v));
         System.out.println(hm);
     }
