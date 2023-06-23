5607f05bfa9ca5f3b80f8faa150381e35bf53c75
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-06-23 11:55:41 -0500

    .

---
+++ java/Collections[Set]/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package Collects;
+
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String s = "srihari";
+
+        Set<Character> sc = new TreeSet<>();
+
+
+        for(int i=s.length()-1;i>=0;i--)
+            sc.add(s.charAt(i));
+        s ="";
+
+        for(char ct:sc)
+            s+=ct;
+
+        System.out.println(s);
+
+
+
+    }
+
+
+
+}
