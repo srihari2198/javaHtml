49d83ecc4cfe322e8ef15c95f2206045229f0f70
srihari2198 <100386160+srihari2198@users.noreply.github.com>
2023-07-07 11:41:28 -0500

    .

---
+++ rest-demo/src/main/java/com/srihari/restdemo/model/JustString.java
@@ -0,0 +1,13 @@
+package com.srihari.restdemo.model;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+public class JustString {
+
+    private String name;
+}
---
+++ rest-demo/src/main/java/com/srihari/restdemo/service/DemoService.java
@@ -0,0 +1,35 @@
+package com.srihari.restdemo.service;
+
+import com.srihari.restdemo.model.JustString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    private List<String> js= new ArrayList<>();
+
+    public List<String> getNames(){
+        return js;
+    }
+
+    public boolean postName(String s){
+        js=new ArrayList<>();
+        return js.add(s);
+    }
+
+    public boolean putName(String s){
+        return js.add(s);
+    }
+
+    public boolean deleteName(String s){
+
+        return js.remove(s);
+
+
+
+    }
+}
